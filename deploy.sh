da7712e097b1389b3b8a150c1a058ed5c256822c
boblaublaw <github@sublimation.org>
2016-08-11 15:12:20 -0700

    changing config parms

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 
 #!/bin/bash
 
-s3cmd --config=~/.s3joeboyle sync pub/. s3://joeboyle.com/ --exclude '.DS_Store'
-s3cmd setacl --config=~/.s3joeboyle s3://joeboyle.com/ --acl-public --recursive
+s3cmd -c ~/.s3joeboyle sync pub/. s3://joeboyle.com/ --exclude '.DS_Store'
+s3cmd setacl -c ~/.s3joeboyle s3://joeboyle.com/ --acl-public --recursive
